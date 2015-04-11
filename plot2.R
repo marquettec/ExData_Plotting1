2cd92a353bc1fe933208588ebed0320b1a137d06
Christophe MARQUETTE <cmarquette.externe@pagesjaunes.fr>
2015-04-11 19:42:49 +0200

    Plot2

---
+++ plot2.R
@@ -0,0 +1,31 @@
+install.packages(c('data.table', 'sqldf', 'reshape2'))
+library('data.table')
+#library('sqldf')
+library('reshape2')
+library('dplyr')
+
+# Getting data
+if (!file.exists('./data')) {
+  dir.create('./data')
+}
+if (!file.exists('./data/exdata-data-household_power_consumption.zip')) {
+  fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(fileUrl, destfile='./data/exdata-data-household_power_consumption.zip', method='curl')
+}
+if (!file.exists('./data/household_power_consumption.txt')) {
+  unzip('./data/exdata-data-household_power_consumption.zip', exdir='./data')
+}
+
+dataFile <- './data/household_power_consumption.txt'
+data <- fread(dataFile, na.strings="?", sep=';', colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+data$Date <- as.Date(data$Date, ,format='%d/%m/%Y')
+
+subset <- subset(data, Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
+subset <- mutate(subset, DateTime=as.POSIXct(strptime(paste(Date, ' ', Time), '%Y-%m-%d %H:%M:%S')))
+
+subset$Global_active_power = as.double(subset$Global_active_power)
+
+png(file='plot2.png', width=480, height=480)
+with(subset, plot(Global_active_power ~ DateTime, type='n', xlab='', ylab='Global Active Power (kilowatts)'))
+with(subset, lines(Global_active_power ~ DateTime, lwd=1))
+dev.off()
